8dbf41a7eb8349be9292ca25d218c4d10b393aad
Mikio-Shimazue <shimazun@icloud.com>
2023-02-13 20:56:44 +0900

    初回登録

---
+++ schedule-book/schedule_bookApp.swift
@@ -0,0 +1,17 @@
+//
+//  schedule_bookApp.swift
+//  schedule-book
+//
+//  Created by 島津江幹雄 on 2023/02/13.
+//
+
+import SwiftUI
+
+@main
+struct schedule_bookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
